b69cfb5dbf1342b8a99b290a0a09691ebd8daec7
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-23 01:57:14 +0600

    added

--- 06.c
+++ 06.c
@@ -5,3 +5,33 @@ int main() {
 
     printf("Enter Day Number of the Week: ");
     scanf("%d", &a);
+
+    switch (a) {
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+        default:
+            printf("Invalid day number");
+            break;
+    }
+
+    return 0;
+}
