fb0797f5405d5933410892387308f394a4214c96
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-28 21:10:01 +0600

    added

--- 10.c
+++ 10.c
@@ -3,3 +3,13 @@
 float isSum(float x, float y) {
     return x + y;
 }
+
+float isMax(float x, float y) {
+    float large;
+    if (x > y) {
+        large = x;
+    } else {
+        large = y;
+    }
+    return large;
+}
