8b9602c9ddb9544e5930465796eecac270f1fb27
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-28 21:13:51 +0600

    added

--- 11.c
+++ 11.c
@@ -8,3 +8,18 @@ int main() {
     char arr1[ARRAY_SIZE][STRING_LENGTH] = {
         "Volvo", "BMW", "Ford", "Mazda"
     };
+
+    // Modify the first element
+    strcpy(arr1[0], "Opel");
+
+    // Print the first element
+    printf("First Element Name: %s\n", arr1[0]);
+
+    // Print the size of the first element (length of the string)
+    printf("First Element Size: %lu\n", strlen(arr1[0]));
+
+    // Print the array size
+    printf("Array Size: %lu\n", sizeof(arr1) / sizeof(arr1[0]));
+
+    return 0;
+}
