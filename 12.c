0c46f5609b891439d4c11d930142029b7fc8e5be
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-30 08:26:15 +0600

    added

--- 12.c
+++ 12.c
@@ -7,3 +7,10 @@ int main() {
 
     printf("Enter the number of elements: ");
     scanf("%d", &a);
+
+    // Allocate memory for array of char pointers
+    char **arr1 = (char **)malloc(a * sizeof(char *));
+    if (arr1 == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
